7ed2a3734facb67c112bac2623063412c1434fb0
bcwojcik <bcwojcik@uwm.edu>
2019-11-06 14:16:26 -0600

    Added quickSort and switched to an object oriented approach.

--- src/SortingVisualizer.java
+++ src/SortingVisualizer.java
@@ -1,24 +1,22 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
-enum Algorithm {
-	SELECTION, INSERTION, QUICK, MERGE;
-}
+import java.util.Stack;
 
 class SortingVisualizer {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Welcome to sorting visualizer!\n\nPress enter to sort step by step, or enter q to quit.");
-		int length = inputLength(scan);
-		int[] arr = initArray(length);
-		visArray(arr);
-		Algorithm al = inputAlgorithm(scan);
-		sortSteps(arr, scan, al);
-		scan.close();
+	
+	private static int[] arr;
+	private static Algorithm al;
+	
+	SortingVisualizer() {
+		initArray(10);
+	}
+	
+	SortingVisualizer(int length) {
+		initArray(length);
 	}
 
-	private static int[] initArray(int length) {
-		int[] arr = new int[length];
+	private static void initArray(int length) {
+		arr = new int[length];
 		for(int i = 0; i < length; ++i) {
 			arr[i] = i+1;
 		}
@@ -28,10 +26,9 @@ class SortingVisualizer {
 			arr[j] = arr[r];
 			arr[r] = temp;
 		}
-		return arr;
 	}
 
-	private static void visArray(int[] arr) {
+	void visArray() {
 		int s = arr.length;
 		for(int i = 0; i < arr.length; ++i) {
 			for(int j = 0; j < arr.length; ++j) {
@@ -44,22 +41,7 @@ class SortingVisualizer {
 		System.out.print("\n");
 	}
 
-	private static int inputLength(Scanner scan) {
-		System.out.println("\nPlease enter an array length: ");
-		int length = 0;
-		while(length < 2) {
-			try {
-				length = scan.nextInt();
-				if(length < 2) System.out.println("Please enter an integer greater than 1: ");
-			}catch(InputMismatchException e) {
-				System.out.println("Please enter an integer greater than 1: ");
-			}
-			scan.nextLine();
-		}
-		return length;
-	}
-
-	private static Algorithm inputAlgorithm(Scanner scan) {
+	void inputAlgorithm(Scanner scan) {
 		System.out.println("Please enter a sorting algorithm [selection, insertion, quick, merge]: ");
 		Algorithm al = null;
 		String s = "";
@@ -75,51 +57,57 @@ class SortingVisualizer {
 				System.out.println("Please enter either [selection, insertion, quick, merge]: ");
 			}
 		}
-		return al;
+		SortingVisualizer.al = al;
 	}
 
-	private static void sortSteps(int[] arr, Scanner scan, Algorithm al) {
+	void sortSteps(Scanner scan) {
 		System.out.println("\nPress enter for each step of sorting:");
 		int at = 0;
-		while(!inOrder(arr)) {
-			String s = scan.nextLine();
-			if(s.equalsIgnoreCase("q")) break;
+		int qhi = arr.length-1;
+		int qlo = 0;
+		Stack<Integer> qstack = new Stack<Integer>();
+		qstack.push(qlo);
+		qstack.push(qhi);
+		while(!inOrder()) {
+			String str = scan.nextLine();
+			if(str.equalsIgnoreCase("q")) break;
 			else {
+				System.out.println("Steps: " + (at+1));
 				switch(al) {
 				case SELECTION:
-					arr = selectionSort(arr, at);
+					selectionSort(at);
 					break;
 				case INSERTION:
-					arr = insertionSort(arr, at+1);
+					insertionSort(at+1);
 					break;
 				case QUICK:
+					quickSort(qstack);
 					break;
 				case MERGE:
 					break;
 				default:
 					throw new IllegalArgumentException("ERROR: Invalid algorithm!");
 				}
-				visArray(arr);
 			}
+			visArray();
 			++at;
 		}
 	}
 	
-	private static boolean inOrder(int[] arr) {
+	private static boolean inOrder() {
 		for(int i = 0; i < arr.length-1; ++i) if(arr[i] >= arr[i+1]) return false;
 		return true;
 	}
 	
-	private static int[] selectionSort(int[] arr, int at) {
+	private static void selectionSort(int at) {
 		int min = at;
 		for (int i = at+1; i < arr.length; i++) if (arr[i] < arr[min]) min = i; 
 		int temp = arr[min]; 
 		arr[min] = arr[at]; 
 		arr[at] = temp; 
-		return arr;
 	}
 	
-	private static int[] insertionSort(int[] arr, int at) {
+	private static void insertionSort(int at) {
 		int i = at-1;
 		int min = arr[at];
 		while(i >= 0 && arr[i] > min) {
@@ -127,6 +115,39 @@ class SortingVisualizer {
 			--i;
 		}
 		arr[i+1] = min;
-		return arr;
+	}
+	
+	/*
+	 * Referenced: https://www.geeksforgeeks.org/iterative-quick-sort/
+	 */
+	private static void quickSort(Stack<Integer> qstack) {
+		int qhi = qstack.pop();
+        int qlo = qstack.pop();
+        int part = quickSortPartition(qhi, qlo); 
+        if (part - 1 > qlo) { 
+            qstack.push(qlo);
+            qstack.push(part-1); 
+        } 
+        if (part + 1 < qhi) { 
+            qstack.push(part+1); 
+            qstack.push(qhi);
+        }
+	}
+	
+	private static int quickSortPartition(int hi, int lo) {
+		int piv = arr[hi];
+		int s = lo-1;
+		for(int i = lo; i < hi; ++i) {
+			if(arr[i] < piv) {
+				++s;
+				int temp = arr[s];
+				arr[s] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		int temp = arr[s+1];
+		arr[s+1] = arr[hi];
+		arr[hi] = temp;
+		return s+1;
 	}
 }
